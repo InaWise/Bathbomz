0c200dc20c8dea0bf5a88310bf173fb7a7219d8a
InaWise <Blissful.simplicity16@gmail.com>
2021-07-25 14:43:54 -0400

    current update

--- server/config/seeds.js
+++ server/config/seeds.js
@@ -23,7 +23,7 @@ db.once('open', async () => {
         'For the man who is tired of crunching numbers and sitting in a corner cubicle. This blend of Dead Sea Salt and citrus is just the invigorating fizz you just might need. ',
       image: 'cookie-tin.jpg',
       category: categories[0]._id,
-      price: 10.99,
+      price: 100.99,
       quantity: 500
     },
     {
@@ -76,7 +76,7 @@ db.once('open', async () => {
       category: categories[2]._id,
       description:
         'For the man that could use a chill session in his life, who’s just tired of everything. This blend of lavender and chamomile is just the fizz that your body is crying out for.',
-      image: 'tablet.jpg',
+      image: '',
       price: 10.99,
       quantity: 30
     },
@@ -94,7 +94,7 @@ db.once('open', async () => {
       category: categories[4]._id,
       description: 'Black Lava salt that is infused with activated charcoal which assists the body in removing harmful impurities, Hemlock needles oozing with Vitamin C, organic Darjeeling loose leaf tea with a slight flowery note and pure essential oils to infuse your senses and take you away to the great outdoors.',
       image: 'spinning-top.jpg',
-      price: 10.99,
+      price: 100.99,
       quantity: 1000
     },
     {
@@ -103,7 +103,7 @@ db.once('open', async () => {
       description:
         'made with soothing organic coconut oil that wraps your skin in silkiness, organic hibiscus flower which contains high levels of anti-oxidants and pure essential oils to infuse your senses to whisk you to a beach far away',
       image: 'plastic-horses.jpg',
-      price: 15.99,
+      price: 10.99,
       quantity: 1000
     },
     {
@@ -112,14 +112,14 @@ db.once('open', async () => {
       description:
         'Our Lumberjack Spice bath bombs are a luxury for the men out there. They are made with organic peppercorns to open circulation, organic Darjeeling loose leaf tea with a slight flowery note, organic cloves, and cinnamon to get you up and going and pure essential oils to infuse your senses and take you away to the great outdoors.',
       image: 'teddy-bear.jpg',
-      price: 7.99,
+      price: 10.99,
       quantity: 100
     },
     {
       name: 'Seagrass Tea',
       category: categories[4]._id,
       description:
-        'Our Seagrass bath bombs are absolutely amazing, soothing yet calming. Made with soothing organic coconut oil that wraps your skin in silkiness, organic lemongrass which has healing and glowing properties for the skin, organic green tea which has antioxidant, anticancer, antibacterial, and antiviral properties for your skin, organic mint can help improve your skin's condition, and pure essential oils to infuse your senses to whisk you to a beach far away.',
+        'Our Seagrass bath bombs are absolutely amazing, soothing yet calming. Made with soothing organic coconut oil that wraps your skin in silkiness, organic lemongrass which has healing and glowing properties for the skin, organic green tea which has antioxidant, anticancer, antibacterial, and antiviral properties for your skin, organic mint can help improve your skin condition and pure essential oils to infuse your senses to whisk you to a beach far away.',
       image: 'alphabet-blocks.jpg',
       price: 9.99,
       quantity: 600
